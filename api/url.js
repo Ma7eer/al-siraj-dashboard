e47fb1dd2822dbbf97dc4bb9d945d9aaf49c4888
Ma7eer <maher.alkendi2@gmail.com>
2020-03-23 22:42:32 +0400

    bug: spelled NODE_ENV wrong

--- api/url.js
+++ api/url.js
@@ -1,6 +1,6 @@
 let API_BASE_URL = "";
 
-if (process.env.NOE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   API_BASE_URL = "https://guarded-ocean-88921.herokuapp.com";
 } else {
   API_BASE_URL = "http://localhost:3001";
