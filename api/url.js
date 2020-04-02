977448d5944a3d73cb9b3666a46b9cb0c75ceed7
Ma7eer <maher.alkendi2@gmail.com>
2020-04-02 19:38:38 +0400

    changed url

--- api/url.js
+++ api/url.js
@@ -1,7 +1,8 @@
 let API_BASE_URL = "";
 
 if (process.env.NODE_ENV === "production") {
-  API_BASE_URL = "https://guarded-ocean-88921.herokuapp.com";
+  API_BASE_URL = "https://shrouded-everglades-46659.herokuapp.com/";
+  // API_BASE_URL = "https://guarded-ocean-88921.herokuapp.com";
 } else {
   API_BASE_URL = "http://localhost:3001";
 }
